481198b4db3acb5f0d1c5584063743a65892aaeb
JMErickson17 <JMEricksonDev@gmail.com>
2017-08-11 17:26:02 -0400

    Add AuthService.swift containing user related variables and methods

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+/* -------------------- Segues -------------------- */
+
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+/* -------------------- User Defaults -------------------- */
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
+/* -------------------- URLs -------------------- */
+
+let BASE_URL = "https://ios-chat-api.herokuapp.com/v1/"
+let REGISTER_URL = "\(BASE_URL)account/register"
