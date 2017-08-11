fdc1f8cb28984dc16a16be8f55b4122b91793f44
JMErickson17 <JMEricksonDev@gmail.com>
2017-08-11 13:40:21 -0400

    Add Login view and view controller, add UI components to LoginVC view

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Justin Erickson on 8/11/17.
+//  Copyright © 2017 Justin Erickson. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
