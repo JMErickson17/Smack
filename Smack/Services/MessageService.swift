cde2971b4b92d5dce5c512c1f53e4d03c734e175
JMErickson17 <JMEricksonDev@gmail.com>
2017-09-02 13:00:49 -0400

    Add MessageService to retrieve messages and channels from API

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class MessageService {
     
@@ -15,5 +17,33 @@ class MessageService {
     // Stores the channels
     var channels = [Channel]()
     
-    
+    func findAllChannels(completion: @escaping CompletionHandler) {
+        Alamofire.request(GET_CHANNELS_URL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                
+//                Swift 4
+                do {
+                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
+                } catch let error {
+                    debugPrint(error as Any)
+                }
+//                SwiftyJSON
+//                if let json = JSON(data: data).array {
+//                    for item in json {
+//                        let name = item["name"].stringValue
+//                        let channelDescription = item["description"].stringValue
+//                        let id = item["_id"].stringValue
+//                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+//                        self.channels.append(channel)
+//                    }
+//                    completion(true)
+//                }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
 }
